72778d4135607dbc97ca88c792e4d60e96e6f5b9
Kevin  Carvalho de Jesus <kevinjesus@Kevins-MacBook-Air.local>
2025-01-24 21:20:45 -0300

    :wrench: add environent variable support

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,13 +1,23 @@
 use ethers::prelude::*;
 use ethers::providers::{Provider, Http};
 use std::sync::Arc;
+use dotenv::dotenv;
 use std::env;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let contract_address = "0x5fbdb2315678afecb367f032d93f642f64180aa3".parse::<Address>()?;
+    dotenv().ok();
+
+    let contract_address = env::var("CONTRACT_ADDRESS")
+        .expect("CONTRACT_ADDRESS must be set")
+        .parse::<Address>()?;
+
+    let rpc_address = env::var("RPC_ADDRESS").expect("RPC_ADDRESS must be set");
+    let account_private_key = env::var("ACCOUNT_PRIVATE_KEY").expect("ACCOUNT_PRIVATE_KEY must be set");
+
     abigen!(IERC721, "./MyContract.json");
-    let rpc_url = format!("http://127.0.0.1:8545/{}", "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80");
+
+    let rpc_url = format!("{}/{}", rpc_address, account_private_key);
     let provider = Provider::<Http>::try_from(rpc_url.as_str())?;
     let provider = Arc::new(provider);
     let contract = IERC721::new(contract_address, provider.clone());
