fbbda2f977e7eeb75779da3091fff5618da2f178
KPMGE <jesuskevin254@gmail.com>
2025-02-05 23:27:37 -0300

    :sparkles: update app name

--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Dapp voting system',
+  description: 'A Dapp for a voting system!',
 }
 
 export default function RootLayout({
